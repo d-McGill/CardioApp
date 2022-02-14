fc7dd20648bb417a2e36366c336f6955183bee54
d-McGill <dmcgillz@icloud.com>
2022-02-14 11:33:19 +0000

    firebase

---
+++ src/firebase/database.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+// firebaseconfig here
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIYZAArKg-0WbVGz8IWtMFZOmeiz-3ADM",
+  authDomain: "cardioapp-936f2.firebaseapp.com",
+  projectId: "cardioapp-936f2",
+  storageBucket: "cardioapp-936f2.appspot.com",
+  messagingSenderId: "820336146650",
+  appId: "1:820336146650:web:1ef5acfcd3c0433e5f8de3"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const firebaseAuthentication = firebase.auth();
+const firebaseFireStore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { firebaseAuthentication, firebaseFireStore, timestamp };
