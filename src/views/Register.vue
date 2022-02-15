632ffb38931605c32e8a3a69b000b0a993c52951
d-McGill <dmcgillz@icloud.com>
2022-02-15 15:07:36 +0000

    Register Logic with firebase Authentication - email notification - redirect - add user to user collection

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,105 +1,89 @@
 <template>
   <el-row class="marginTB">
-    <el-col :span="8"></el-col>
-    <el-col :span="8">
-      <h1>Register for an account</h1>
-
-    <el-form label-width="95px" class="demo-ruleForm" @submit.prevent>
-    <h2>Register</h2>
-
-    <el-form-item label="First Name">
-      <el-input
-        type="text"
-        placeholder="First Name"
-        required
-        autocomplete="off"
-        v-model="firstName"
-      ></el-input>
-    </el-form-item>
-
-    <el-form-item label="Last Name">
-      <el-input
-        type="text"
-        placeholder="Surname"
-        required
-        autocomplete="off"
-        v-model="surname"
-      ></el-input>
-    </el-form-item>
-
-    <el-form-item label="Display Name">
-      <el-input
-        type="text"
-        placeholder="Display Name"
-        required
-        autocomplete="off"
-        v-model="displayName"
-      ></el-input>
-    </el-form-item>
-
-    <el-form-item label="Email">
-      <el-input
-        type="email"
-        placeholder="email"
-        required
-        autocomplete="off"
-        v-model="email"
-      ></el-input>
-    </el-form-item>
-
-    <el-form-item label="Password" prop="pass">
-      <el-input
-        type="password"
-        placeholder="password"
-        required
-        autocomplete="off"
-        show-password
-        v-model="password"
-      ></el-input>
-    </el-form-item>
-
-    <el-form-item label="Confirm Password" prop="pass">
-      <el-input
-        type="password"
-        placeholder="password"
-        required
-        autocomplete="off"
-        show-password
-        v-model="confirmPassword"
-      ></el-input>
-    </el-form-item>
-
-    <div v-if="errorRegistration">
-      <el-button plain type="danger" disabled icon="el-icon-error">
-        {{ errorRegistration }}
-      </el-button>
+    <el-col :span="7">
+      <div v-if="errorRegistration" class="errorInfo">
+        <h1>Form Vaildation Error</h1>
+        <div class="innerError">
+        <h2>Please rectify the following:</h2>
+        <br/>
+        <h3>{{ errorRegistration }}</h3>
+        <br/>
+        </div>
     </div>
+    </el-col>
+    <el-col :span="10">
+      <h1>Register for an account</h1>
+    <el-form class="demo-ruleForm" @submit.prevent>
+       <div class="formWrapper">
+  
+
 
     <el-form-item>
-      <el-button type="success" style="margin: auto" @click="register"
-        >Register</el-button
-      >
+      <label>Username *</label>
+      <el-input type="email" placeholder="Email must be a vaild email address" required autocomplete="off" v-model="email"></el-input>
     </el-form-item>
+
+
+    <el-form-item prop="pass">
+       <label>Password *</label>
+      <el-input type="password" placeholder="Password must be at least 6 characters long" required autocomplete="off" show-password v-model="password"></el-input>
+    </el-form-item>
+
+    <el-form-item prop="pass">
+       <label>Confirm Password *</label>
+      <el-input type="password" placeholder="Must match password" required autocomplete="off" show-password v-model="confirmPassword"></el-input>
+    </el-form-item>
+
+  <el-form-item>
+    <label>Name *</label>
+      <el-input type="text" placeholder="Enter your full name" required autocomplete="off" v-model="name"></el-input>
+    </el-form-item>
+
+    <el-form-item>
+      <label>Full Address *</label>
+      <el-input type="text" placeholder="Enter your full address" required autocomplete="off" v-model="address"></el-input>
+    </el-form-item>
+
+     <el-form-item>
+      <label>Phone Number *</label>
+      <el-input type="text" placeholder="Enter you full phone number including area code" required autocomplete="off" v-model="number"></el-input>
+    </el-form-item>
+
+      <el-form-item>
+      <label>Institutional Affiliation *</label>
+      <el-input type="text" placeholder="enter full institution name" required autocomplete="off" v-model="institute"></el-input>
+    </el-form-item>
+
+  </div>
+    <el-form-item>
+      <el-button type="primary" style="margin: auto" @click="register">Register</el-button>
+    </el-form-item>
+
   </el-form>
 
-      </el-col>
-    <el-col :span="8"></el-col>
+</el-col>
+    <el-col :span="7">
+    </el-col>
   </el-row>
 
 </template>
 <script>
 import { ref, watch } from "vue";
 import { useRouter } from "vue-router";
-import { firebaseAuthentication } from "@/firebase/database";
+import { firebaseAuthentication} from "@/firebase/database";
+import firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
 
 export default {
   name: "register",
   emits: ["register-clicked"],
 
   setup() {
-    const firstName = ref("");
-    const surname = ref("");
-    const displayName = ref("");
+    const name = ref("");
+    const address = ref("");
+    const number = ref("");
+    const institute = ref("");
     const email = ref("");
     const password = ref("");
     const confirmPassword = ref("");
@@ -122,8 +106,11 @@ export default {
     function register() {
       const info = {
         email: email.value,
+        name: name.value,
         password: password.value,
-        displayName: displayName.value,
+        phoneNumber: number.value,
+        address: address.value,
+        institute: institute.value
       };
 
       if (!errorRegistration.value) {
@@ -131,12 +118,23 @@ export default {
           .createUserWithEmailAndPassword(info.email, info.password)
           .then(
             (userCredentials) => {
-              return userCredentials.user
+            return userCredentials.user
                 .updateProfile({
-                  displayName: info.displayName,
+                  displayName: info.name,
                 })
                 .then(() => {
-                  router.replace("register");
+             firebase.firestore().collection('users').doc(userCredentials.user.uid).set({
+                     email: info.email,
+                     name: info.name,
+                     password: info.password,
+                     phoneNumber: info.phoneNumber,
+                     address: info.address,
+                     institute: info.institute                     
+                     });
+
+                  userCredentials.user.sendEmailVerification(); 
+                  router.replace("login");
+                
                 });
             },
             (error) => {
@@ -147,9 +145,10 @@ export default {
     }
 
     return {
-      firstName,
-      surname,
-      displayName,
+      name,
+      address,
+      number,
+      institute,
       email,
       password,
       confirmPassword,
@@ -160,14 +159,47 @@ export default {
 };
 </script>
 <style> 
-  .marginTB{ margin: 5% 0};
+  .marginTB{ margin: 2% 0};
 </style>
 <style scoped>
     .errorInfo{
-      font-size: 14px;
+      font-size: 14px !important;
       color: red;
-      margin: -10px 0 20px 0px;
+      margin: 5%;
       padding: 0px;
   }
+
+
+  .formWrapper{
+    height:500px;
+    overflow: scroll;
+    box-shadow: 0 0 8px rgba(0,0,0,0.3);
+    padding: 5%;
+   border-radius: 8px;
+   margin:5% 0;
+  }
+
+  .el-button {
+  width:40%;
+  font-size: 18pt;
+  padding: 5% 8%;
+}
+.el-button  a{
+  text-decoration: none;
+  color:#fff;
+}
+
+.errorInfo{
+  border: 1px solid  #FF0000;
+  padding:5%;
+  border-radius: 8px;
+}
+
+.innerError{
+  background-color: #fff; 
+  border-radius: 8px;
+  padding: 5%;
+
+}
 </style>
 
