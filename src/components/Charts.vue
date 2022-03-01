dd18928516dd89c02b5e6d3a36cd1940a0da2e5b
d-McGill <77538732+d-McGill@users.noreply.github.com>
2022-03-01 15:09:26 +0000

    test

--- src/components/Charts.vue
+++ src/components/Charts.vue
@@ -1,6 +1,7 @@
 <template>
 <el-row style="padding:0 15%;" >
   <el-col :span="12" >
+    <p>TEST</p>
     <h3>X - axis</h3>
   <!--    X DROP DOWN   -->
     <el-select v-model="dataTerm.x" class="m-2" placeholder="Select" size="large">
