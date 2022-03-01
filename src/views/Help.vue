c896fdd97765d311ca2a89448aba3ad86edce404
d-McGill <77538732+d-McGill@users.noreply.github.com>
2022-03-01 10:47:33 +0000

    hep fix

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,13 +1,46 @@
 <template>
   <div>
-    <h1>HELP PAGE</h1>
+    <HelpInfo :post="Welcome" />
+    <HelpInfo :post="post" v-for="(post, index) in Sections" :key="index"/>
   </div>
 </template>
+
 <script>
-
-
-export default {
-
-  }
-
+  import HelpInfo from '../components/HelpInfo.vue';
+  export default {
+    components: {
+      HelpInfo
+    },
+    data(){
+      return {
+        Welcome: {
+          title: 'Help Page',
+          description: 'Stuck on something?',
+          IntroSection: true,
+        },
+        Sections: [
+          {
+            title: 'The Home page',
+            description: 'What it does. (more detail on what it does)',
+            link: 'Home'
+          },
+          {
+            title: 'The Dashboard page',
+            description: 'What it does. (more detail on what it does)',
+            link: 'Dashboard'
+          },
+          {
+            title: 'The Learning page',
+            description: 'What it does. (more detail on what it does)',
+            link: 'QuerySearch'
+          },
+          {
+            title: 'The Profile page',
+            description: 'What it does. (more detail on what it does)',
+            link: 'Profile'
+          },
+        ]
+      };
+    }
+  };
 </script>
