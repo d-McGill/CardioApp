60f9f3d0ac3604615e3ed0d24a8fda54f0c15629
d-McGill <77538732+d-McGill@users.noreply.github.com>
2022-03-03 17:38:51 +0000

    update

--- tests/unit/FormClicks.spec.js
+++ tests/unit/FormClicks.spec.js
@@ -1,18 +1,19 @@
-import Login from '@/views/Login.vue';
-import Register from '@/views/Register.vue';
-import Charts from '@/components/Charts.vue';
-import { mount } from '@/vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
+import Login from '@/views/Login.vue'
+import Register from '@/views/Register.vue'
+import Charts from '@/components/Charts.vue'
+
 
 describe('Form Emmits && Clicks', () => {
     it("Login button", async () => {
-        const wrapper = mount(Login)
+        const wrapper = shallowMount(Login)
         const button = wrapper.find('#loginButton')
         button.trigger('click');
         expect(wrapper.emitted('login-clicked'))
   })
 
   it("Register button", async () => {
-    const wrapper = mount(Register)
+    const wrapper = shallowMount(Register)
     const button = wrapper.find('.registerButton')
     button.trigger('click');
     expect(wrapper.emitted('register-clicked'))
@@ -20,7 +21,7 @@ describe('Form Emmits && Clicks', () => {
 })
 
 it("Graph button", async () => {
-    const wrapper = mount(Charts)
+    const wrapper = shallowMount(Charts)
     const button = wrapper.find('.graphButton')
     button.trigger('click');
     
