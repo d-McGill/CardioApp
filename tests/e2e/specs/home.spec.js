0e3b833594eb71cd4007e22f9eb76d2f8de4ec28
d-McGill <77538732+d-McGill@users.noreply.github.com>
2022-03-03 17:07:15 +0000

    url update

--- tests/e2e/specs/home.spec.js
+++ tests/e2e/specs/home.spec.js
@@ -11,17 +11,17 @@ describe('Home Page Button tests', () => {
   it('Login Button Destination', () => {
     cy.visit('/')
     cy.get('.loginButton').click()
-    cy.url().should('be.equal', `${Cypress.config("baseUrl")}login`)
+    cy.url().should('be.equal', `http://localhost:8080/login`)
   })
   it('Register Button Destination', () => {
     cy.visit('/')
     cy.get('.RegisterButton').click()
-    cy.url().should('be.equal', `${Cypress.config("baseUrl")}register`)
+    cy.url().should('be.equal', `http://localhost:8080/register`)
   })
   it('Help Button Destination', () => {
     cy.visit('/')
     cy.get('.helpButton').click()
-    cy.url().should('be.equal', `${Cypress.config("baseUrl")}help`)
+    cy.url().should('be.equal', `http://localhost:8080/help`)
   })
   it('Dark Mode', () => {
     cy.visit('/')
