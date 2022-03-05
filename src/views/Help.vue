a3c5f238c996497da4b9fe5d6d435ffccd95e075
d-McGill <77538732+d-McGill@users.noreply.github.com>
2022-03-05 16:21:46 +0000

    styling update

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -2,36 +2,36 @@
   <el-container>
     <el-aside>  
       <div class="menuItems">
-        <el-button @click="displayAbout" type="info" round>About</el-button>
-        <el-button @click="displayLearning" type="info" round>Learning</el-button>
-        <el-button @click="displayDashboard" type="info" round>Dashboard</el-button>
-        <el-button @click="displayProfile" type="info" round>Profile</el-button>
+        <el-button @click="displayAbout" type="info" class="infoButton">About</el-button>
+        <el-button @click="displayLearning" type="info" class="infoButton">Learning</el-button>
+        <el-button @click="displayDashboard" type="info" class="infoButton">Dashboard</el-button>
+        <el-button @click="displayProfile" type="info" class="infoButton">Profile</el-button>
       </div>
     </el-aside>
 
     <el-main>
       <div class="aboutSection" v-if="about == 'true'">
-        <h1> About </h1>
+        <h1>Cardio App</h1>
+        <p class="info">An application that brings all relevant research cardiomyopathy data in one place.
+         Analyse, contribute, and learn with our and external data sets.
+         Create an account to get started or register in a few easy steps. In the meantime here are some definitions.</p>
         <FactsAPI />
       </div>
 
       <div class="learningSection" v-if="learning == 'true'">
         <h1> Learning </h1>
-        <br><br>
-        <p>The learning page allows users to independently learn about different HCM gene mutations and different types of Cardiomyopathy. By selecting a certain type of cardiomyopathy ranging from Hypertrophic Cardiomyopathy, Noncompaction Cardiomyopathy, , Restrictive Cardiomyopathy, Atrial Cardiomyopathy, Dilated Cardiomyopathy and more users will be able to the clinical synopsis for that selected Cardiomyopathy. <br>
+        <p class="info">The learning page allows users to independently learn about different HCM gene mutations and different types of Cardiomyopathy. By selecting a certain type of cardiomyopathy ranging from Hypertrophic Cardiomyopathy, Noncompaction Cardiomyopathy, , Restrictive Cardiomyopathy, Atrial Cardiomyopathy, Dilated Cardiomyopathy and more users will be able to the clinical synopsis for that selected Cardiomyopathy. <br>
         <br>Users can also select from different HCM gene mutations ranging from, MHY7, MYBPC3, TNNT2, TPM1, MYL2 and more. By selecting one users will be able to see a descriptive mutation description alongside associated diseases and terms </p>
       </div>
 
       <div class="dashboardSection" v-if="dashboard == 'true'">
         <h1> Dashboard </h1>
-        <br><br>
-        <p>The dashboard page contains a section to allow users to upload data regarding gene mutations and different cardiomyopathies which will be stored for your user. Data uploaded will be represented in the visualise tab of the dashboard after selecting from the pre set options based on the data uploaded a graph will be plotted, users can also select the type of the chart to display the data in.</p>
+        <p class="info">The dashboard page contains a section to allow users to upload data regarding gene mutations and different cardiomyopathies which will be stored for your user. Data uploaded will be represented in the visualise tab of the dashboard after selecting from the pre set options based on the data uploaded a graph will be plotted, users can also select the type of the chart to display the data in.</p>
       </div>
 
       <div class="profileSection" v-if="profile == 'true'">
         <h1> Profile </h1>
-        <br><br>
-        <p>The profile page allows for user management, it allows users to see data they have uploaded and edit or remove it, as well allowing users to edit account information or even delete their accounts. </p>
+        <p class="info">The profile page allows for user management, it allows users to see data they have uploaded and edit or remove it, as well allowing users to edit account information or even delete their accounts. </p>
       </div>
     </el-main>
               
@@ -48,7 +48,7 @@ components: {
 
   setup() {
     const about = ref('true');
-    const learning = ref('false');
+    const learning = ref('ture');
     const dashboard = ref('false');
     const profile = ref('false');
 
@@ -90,11 +90,21 @@ components: {
 
 <style scoped>
 
+
+.info{
+  font-size:25px;
+}
+.infoButton{
+  padding:20px;
+}
+p{
+  text-align: left;
+}
+
 .el-aside {
   background: #eee; 
   height: 40rem;
-  margin-top: 5rem;
-  border-radius: 20px;
+  border-radius: 0 20px 20px 0;
   box-shadow: 0 0 8px rgba(0.0,0.0,0.0,0.2);
 }
 
@@ -114,14 +124,11 @@ components: {
   background: #eee;
   box-shadow: 0 0 8px rgba(0.0,0.0,0.0,0.2); 
   height: 40rem;
-  margin-top: 5rem;
   margin-left: 5rem;
   margin-right: 5rem;
   border-radius: 20px;
   font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-  font-size: 25px;
-  padding-left: 50px;
-  padding-right: 50px;
+  padding:5%;
 }
 
 .dark .el-main {
